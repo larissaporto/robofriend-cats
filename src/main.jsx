dbcc03f43ee92423eaafe04ea794c68e91535ec2
Larissa Porto <larissa.silva86@gmail.com>
2025-03-21 17:14:37 +0100

    Refactor to make it dynamic and read all of the information

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Card from './Card'
+import CardList from './CardList'
 import 'tachyons'
 import { robots } from './assets/robots'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <CardList robots={robots} />
     </>
   </StrictMode>,
 )
