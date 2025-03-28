255b1ae02b628880c45bc49c372bdc7a3202440d
Larissa Porto <larissa.silva86@gmail.com>
2025-03-28 16:05:53 +0100

    Finish the search part and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CardList from './CardList'
 import { robots } from './assets/robots'
 import SearchBox from './SearchBox'
+import './App.css'
 
-const App = () => {
-  return (
-    <>
-      <div className='tc'>
-        <h1>Robot Friend - Cat Version</h1>
-        <SearchBox />
-        <CardList robots={robots} />
-      </div>
-    </>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    return (
+      <>
+        <div className='tc'>
+          <h1 className='f1'>Robot Friend - Cat Version</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
